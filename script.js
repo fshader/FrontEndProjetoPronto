4f2b962ac2eb55716d5ee1babb547dc8f1a3c6e5
Felipe Azevedo <fshader94@gmail.com>
2023-02-13 23:15:59 -0300

    SiteFrontEnd

---
+++ script.js
@@ -0,0 +1,102 @@
+<<<<<<< HEAD
+function initTabNav() {
+=======
+>>>>>>> da39c19530ec3944e9debe4e4a6b3acf5b9e97eb
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if(tabMenu.length && tabContent.length) {
+tabContent[0].classList.add('ativo');
+
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  });
+});
+<<<<<<< HEAD
+}
+}
+initTabNav();
+
+function initAccordion() {
+const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo'
+if (accordionList.length) {
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordion() {
+  this.classList.toggle('ativo');
+  this.nextElementSibling.classList.toggle(activeClass);
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion);
+});
+  }
+}
+
+initAccordion();
+
+
+function initScrollSmooth() {
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+if (linksInternos.length) {
+function scrollToSection (event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+
+  //forma alternativa
+  // const topo = section.offsetTop;
+  // window.scrollTo({
+  //   top: topo, 
+  //   behavior: "smooth",
+  // });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+  }
+}
+initScrollSmooth();
+
+function initAnimacaoScroll() {
+const sections = document.querySelectorAll('.js-scroll');
+if(sections.length) {
+  const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+    if(isSectionVisible) {
+      section.classList.add('ativo');
+    }else {
+      section.classList.remove('ativo');
+    }
+  });
+ }
+
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
+}
+}
+initAnimacaoScroll();
+=======
+}
+>>>>>>> da39c19530ec3944e9debe4e4a6b3acf5b9e97eb
